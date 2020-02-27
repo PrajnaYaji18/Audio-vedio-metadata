155d9d95c8ce7c777e86e21be51ebbb63c665116
PrajnaYaji18 <prajna@amagi.com>
2020-02-27 16:53:50 +0530

    duration_tc logic changed

--- app/models/audio.rb
+++ app/models/audio.rb
@@ -1,5 +1,9 @@
 class Audio < Media
 	def duration_tc(duration)
-		return duration
+		miliseconds = (duration.to_i) % 1000
+		seconds = (duration.to_i) / 1000
+		time = Time.at(seconds).utc.strftime("%H:%M:%S")
+		return time.to_s + ":" + miliseconds.to_s
+
 	end
 end
--- app/models/vedio.rb
+++ app/models/vedio.rb
@@ -1,7 +1,9 @@
 class Vedio < Media
 	def duration_tc(milisecs)
-	seconds = ( milisecs.to_i )/1000
-	Time.at(seconds).utc.strftime("%H:%M:%S")
+	seconds = milisecs.to_i/1000
+	frames = ((milisecs.to_i - (seconds * 1000)) * 3 / 50).round
+	time = Time.at(seconds).utc.strftime("%H:%M:%S")
+	return time.to_s + ":" + frames.to_s
 	end
 
 end
