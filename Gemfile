47db8c2fc7e7b2f5f212c23d68ef9ab92e5f8adf
PrajnaYaji18 <prajna@amagi.com>
2020-03-02 17:36:46 +0530

    API documentation is added

--- Gemfile
+++ Gemfile
@@ -7,7 +7,7 @@ ruby '2.3.8'
 gem 'rails', '~> 5.2.1'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
-
+gem 'apipie-rails'
 gem 'rspec'
 
 gem 'mysql2'
--- app/controllers/api/v1/accounts_controller.rb
+++ app/controllers/api/v1/accounts_controller.rb
@@ -1,6 +1,27 @@
 module Api
   module V1
     class AccountsController < ApplicationController
+      resource_description do
+        short "API for managing Accounts"
+      end
+
+      api :POST, '/accounts', "Create an account" 
+      param :Email, String, :desc => "Email ID", :required => true
+      param :UserName, String, :desc => "User Name", :required => true
+      formats ['json']
+      example '
+      POST /api/v1/accounts
+      {
+    "status": "SUCCESS",
+    "message": "Account created succesfully",
+    "data": {
+        "id": 3,
+        "Email": "Prajna@amagi.com",
+        "UserName": "PrajnaYaji18",
+        "created_at": "2020-03-02T11:12:00.000Z",
+        "updated_at": "2020-03-02T11:12:00.000Z"
+    }
+}'
 
 #Create an account
 #Input: Email and UserName 
