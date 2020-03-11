671262920ddeb15ba9f7ff54548f6167c7a0b5bd
PrajnaYaji18 <prajna@amagi.com>
2020-03-11 16:16:06 +0530

    passenger addded

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,118 +5,54 @@ server '18.222.106.89', port: 22, roles: [:web, :app, :db], primary: true
 set :application, "AddMedia"
 set :repo_url, "https://github.com/PrajnaYaji18/Audio-vedio-metadata"
 set :user,            'ubuntu'
-set :puma_threads,    [4, 16]
-set :puma_workers,    0
-set :assets_roles, [:web, :app] 
-set :pty,             true
-set :use_sudo,        false
-set :stage,           :production
-set :deploy_via,      :remote_cache
-set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
-set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
-set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
-set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
-set :puma_access_log, "#{release_path}/log/puma.error.log"
-set :puma_error_log,  "#{release_path}/log/puma.access.log"
-#set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
-set :puma_preload_app, true
-set :puma_worker_timeout, nil
-set :puma_init_active_record, true  # Change to false when not using ActiveRecord
 set :ssh_options, {
   user: fetch(:user),
   forward_agent: true,
   auth_methods: %w[publickey],
   keys: %w[/home/amagi/Downloads/prajna.pem]
 }
-## Defaults:
-# set :scm,           :git
-# set :branch,        :master
-# set :format,        :pretty
-# set :log_level,     :debug
-# set :keep_releases, 5
 
-## Linked Files & Directories (Default None):
-# set :linked_files, %w{config/database.yml}
-# set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :pty,             true
+set :use_sudo,        false
+set :deploy_via,      :remote_cache
+set :rvm_ruby_version, '2.3.8'
+set :passenger_restart_with_sudo, true
 
-namespace :puma do
-  desc 'Create Directories for Puma Pids and Socket'
-  task :make_dirs do
-    on roles(:app) do
-      execute "mkdir #{shared_path}/tmp/sockets -p"
-      execute "mkdir #{shared_path}/tmp/pids -p"
-    end
-  end
+set :format,        :pretty
+set :log_level,     :debug
+set :keep_releases, 5
 
-  before :start, :make_dirs
-end
+set :linked_dirs,  %w{log tmp/pids tmp/cache}
 
 namespace :deploy do
-  desc "Make sure local git is in sync with remote."
-  task :check_revision do
+  desc 'Symlinks config files for Nginx.'
+  task :nginx_symlink do
     on roles(:app) do
-      unless `git rev-parse HEAD` == `git rev-parse origin/master`
-        puts "WARNING: HEAD is not the same as origin/master"
-        puts "Run `git push` to sync changes."
-        exit
-      end
-    end
+      execute "rm -f /etc/nginx/sites-enabled/default"
+
+      execute "sudo ln -nfs #{current_path}/config/nginx.rb /etc/nginx/sites-enabled/#{fetch(:domain)}"
+      execute "sudo ln -nfs   /config/nginx.rb /etc/nginx/sites-available/#{fetch(:domain)}"
+   end
   end
 
-  desc 'Initial Deploy'
-  task :initial do
+  desc 'Symlinks Secret.yml to the release path'
+  task :secret_symlink do
     on roles(:app) do
-      before 'deploy:restart', 'puma:start'
-      invoke 'deploy'
-    end
+      execute "sudo ln -nfs #{shared_path}/secrets.yml #{release_path}/config/secrets.yml"
+   end
   end
 
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      invoke 'puma:restart'
-    end
-  end
-
-  #before :starting,     :check_revision
-  #after  :finishing,    :compile_assets
+  after  :updating,     :secret_symlink
+  after  :updating,     :nginx_symlink
+  after  :finishing,    :compile_assets
   after  :finishing,    :cleanup
   after  :finishing,    :restart
 end
-set :branch, ENV['BRANCH'] if ENV['BRANCH']
-# ps aux | grep puma    # Get puma pid
-# kill -s SIGUSR2 pid   # Restart puma
-# kill -s SIGTERM pid   # Stop puma
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
-
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
-
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# append :linked_files, "config/database.yml"
-
-# Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for local_user is ENV['USER']
-# set :local_user, -> { `git config user.name`.chomp }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
-
-# Uncomment the following to require manually verifying the host key before first deploy.
-# set :ssh_options, verify_host_key: :secure
+namespace :logs do
+  desc 'Tail application logs'
+  task :tail_app do
+    on roles(:app) do
+      execute "tail -f #{shared_path}/log/#{fetch(:stage)}.log"
+    end
+  end
